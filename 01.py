6ca178623eedef8dd93a5b9c9654d8c515422952
Sofi <szaytseva@fmschool72.ru>
2019-12-24 15:55:16 +0500

    circlesss

--- 01.py
+++ 01.py
@@ -1,3 +1,24 @@
-print()
-print(1)
-print(3)
+import sys
+import random
+from PIL import Image
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+import sys
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        pass
+
+
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
